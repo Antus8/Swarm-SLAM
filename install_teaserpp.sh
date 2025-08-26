a547f76918ca734d2acb3a75e890e7e5d02d0204
Antus8 <antus.longo@gmail.com>
2025-08-26 10:23:28 -0700

    Install forked version of the original Swarm-Slam repos

---
+++ install_teaserpp.sh
@@ -0,0 +1,10 @@
+cd ~/jpl_nebula2_ws/src/localization/Swarm-SLAM
+sudo /usr/bin/python -m pip install -r requirements.txt --break-system-packages
+git clone https://github.com/Antus8/TEASER-plusplus.git
+cd TEASER-plusplus
+git checkout feature/nebula_integration
+mkdir build && cd build
+
+cmake -DTEASERPP_PYTHON_VERSION=3.12 .. && make teaserpp_python
+cd python
+sudo /usr/bin/python -m pip install . --break-system-packages
